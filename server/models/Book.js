3ea1a0a4977e55d3358732c8b544c99014a92957
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-01-26 17:11:10 +0800

    Finished book schema

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,5 +1,87 @@
 const { model, Schema } = require('mongoose');
 
-const bookSchema = new Schema({});
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required']
+    },
+    author: {
+        type: [
+            {
+                name: {
+                    type: String,
+                    required: [true, 'Author name is required']
+                },
+                role: {
+                    type: String,
+                    enum: ['author', 'editor'],
+                    requird: [true, 'Author type is required']
+                }
+            }
+        ],
+        required: true,
+        validate: {
+            validator: function (v) {
+                return Array.isArray(v) && v.length > 0;
+            },
+            message: 'At least one (1) author is required'
+        }
+    },
+    isbn: {
+        type: String
+    },
+    edition: {
+        type: String
+    },
+    publisher: {
+        type: String
+    },
+    published_month: {
+        type: String,
+        enum: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ]
+    },
+    published_day: {
+        type: Number,
+        min: [1, 'Day must be between 1 and 31'],
+        max: [31, 'Day must be between 1 and 31']
+    },
+    published_year: {
+        type: Number,
+        min: [1000, 'Year must be a valid 4-digit year'],
+        max: [9999, 'Year must be a valid 4-digit year']
+    },
+    issue: {
+        type: Number
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: new Date().toISOString()
+    },
+    updatedAt: {
+        type: Date,
+        default: new Date().toISOString()
+    },
+    deletedAt: {
+        type: Date,
+        default: null
+    }
+});
 
 module.exports = model('Book', bookSchema);
