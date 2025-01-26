c21502b1d76c0f02768230c486a7a26af9801f28
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-01-26 17:11:27 +0800

    Finished user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,48 @@
 const { model, Schema } = require('mongoose');
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required']
+    },
+    middleName: {
+        type: String
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required']
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required']
+    },
+    pen_name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: new Date().toISOString()
+    },
+    updatedAt: {
+        type: Date,
+        default: new Date().toISOString()
+    },
+    deletedAt: {
+        type: Date,
+        default: null
+    }
+});
 
 module.exports = model('User', userSchema);
