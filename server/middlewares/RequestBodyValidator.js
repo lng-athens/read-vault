33ec36a0c8f12ed8b36c013f0457b0cff92da324
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-01-26 17:10:46 +0800

    Finished ValidateUserRequestBody middleware

--- server/middlewares/RequestBodyValidator.js
+++ server/middlewares/RequestBodyValidator.js
@@ -1,12 +1,47 @@
 const validator = require('validator');
 
-const isUsername = (value) => {}
+const isUsername = (value) => {
+    const regex = /^[A-Za-z0-9][A-Za-z0-9_.]*$/;
 
-const isName = (value) => {}
+    return regex.test(value);
+}
+
+const isName = (value) => {
+    const regex = /^[A-Za-z]+([ ]?[A-Za-z]+)*$/;
+
+    return regex.test(value);
+}
 
 const ValidateBookRequestBody = (req, res, next) => {};
 
-const ValidateUserRequestBody = (req, res, next) => {};
+const ValidateUserRequestBody = (req, res, next) => {
+    const {
+        firstName,
+        middleName,
+        lastName,
+        username,
+        pen_name,
+        email,
+        password
+    } = req.body;
+
+    let errorFields = [];
+
+    if (firstName && !isName(firstName)) {errorFields.push('firstName')}
+    if (middleName && !isName(middleName))  {errorFields.push('middleName')}
+    if (lastName && !isName(lastName))  {errorFields.push('lastName')}
+    if (username && !isUsername(username)) {errorFields.push('username')}
+    if (pen_name && !isName(pen_name)) {errorFields.push('pen_name')}
+    if (email && !validator.isEmail(email)) {errorFields.push('email')}
+    if (password && !validator.isStrongPassword(password, {minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1})) {errorFields.push('password')}
+
+    if (errorFields.length > 0) {
+        res.status(400);
+        throw new Error(`Invalid fields: ${errorFields.join(', ')}`);
+    }
+
+    next();
+};
 
 module.exports = {
     ValidateBookRequestBody,
