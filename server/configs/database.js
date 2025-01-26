201893ce7f7464e714d74d7e3bc51e5406aa720d
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-01-26 17:10:04 +0800

    Setup database connection

--- server/configs/database.js
+++ server/configs/database.js
@@ -3,6 +3,24 @@ const config = require('./config');
 
 let client;
 
-const connectDB = async () => {};
+const connectDB = async () => {
+    if (!client) {
+        try {
+            client = await MongoClient.connect(config.mongoUri, {
+                serverApi: {
+                    version: ServerApiVersion.v1,
+                    strict: true,
+                    deprecationErrors: true
+                }
+            });
+            console.log('Connected to MongoDB');
+        } catch (error) {
+            console.error('Error connecting to MongoDB: ', error.message);
+            process.exit(1);
+        }
+    }
+
+    return client.db();
+};
 
 module.exports = { connectDB };
