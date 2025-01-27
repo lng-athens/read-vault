d86b4bcb7ac360e1225c77f9d4e12e58fcc2f73d
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-01-27 09:55:30 +0800

    Added sign-in route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {} = require('../middlewares/Authentication');
 const { ValidateUserRequestBody } = require('../middlewares/RequestBodyValidator');
-const { UserSignUp } = require('../controllers/usersController');
+const { UserSignUp, UserSignIn } = require('../controllers/usersController');
 
 router.route('/sign-up').post(ValidateUserRequestBody, UserSignUp);
+
+router.route('/sign-in').post(ValidateUserRequestBody, UserSignIn);
 
 module.exports = router;
