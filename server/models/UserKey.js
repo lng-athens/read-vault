ed705531301435b4403f0334e1f53dd523c0b6dc
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-01-27 09:55:11 +0800

    Setup UserKey schema

--- server/models/UserKey.js
+++ server/models/UserKey.js
@@ -1,5 +1,18 @@
 const { model, Schema } = require('mongoose');
 
-const userkeySchema = new Schema({});
+const userkeySchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required']
+    },
+    privateKeyPEM: {
+        type: String,
+        required: [true, 'Private key is required']
+    }
+});
 
 module.exports = model('UserKey', userkeySchema);
