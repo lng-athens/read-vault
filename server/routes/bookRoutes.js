da81c26e55eca87bc830e2afc2f7eeb00b9c28e9
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-01-29 17:18:22 +0800

    Added AddBook route

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {} = require('../middlewares/Authentication');
-const {} = require('../middlewares/RequestBodyValidator');
-const {} = require('../controllers/booksController');
+const { ValidateBookRequestBody } = require('../middlewares/RequestBodyValidator');
+const { AddBook } = require('../controllers/booksController');
 
-
+router.route('/add').post(ValidateBookRequestBody, AddBook);
 
 module.exports = router;
